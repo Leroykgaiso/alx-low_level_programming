bfcf9db95250fede63e8c9da87501a6c74013a68
Leroykgaiso <Leroykgaiso1@hotmail.com>
2022-12-19 12:06:59 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/* This script prints out a string */
+/**
+ *  * main - Entry point
+ *   *   Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+		char str[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(str);
+
+				return (0);/* The script is successful */
+}
