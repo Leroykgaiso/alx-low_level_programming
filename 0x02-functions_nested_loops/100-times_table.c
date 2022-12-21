3fec349ad2b966cf353f8002acd35942699c9349
Leroykgaiso <Leroykgaiso1@hotmail.com>
2022-12-21 11:42:36 -0800

    print

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,9 @@
  *
  * Return: Number matrix
  */
-	void print_times_table(int n)
+void print_times_table(int n)
 {
 	int x, y, z;
-
 	if (n >= 0 && n <= 14)
 {
 	for (x = 0; x <= n; x++)
@@ -33,18 +32,24 @@
 	_putchar(((z / 10) % 10) + '0');
 	_putchar((z % 10) + '0');
 }
-	else
-{
-	if (y != 0)
+	else if (z > 0)
 {
 	_putchar(',');
 	_putchar(32);
 	_putchar(32);
 	_putchar(32);
-	_putchar(z + '0');
+	_putchar((z % 10) + '0');
+}
+	else if (y > 0)
+{
+	_putchar(',');
+	_putchar(32);
+	_putchar(32);
+	_putchar(32);
+	_putchar(48);
 }
 }
 	_putchar('\n');
-}
+	}
 }
 }
