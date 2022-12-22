23e346f4d9738d05ad874c7330f79a8ae3019763
Leroykgaiso <Leroykgaiso1@hotmail.com>
2022-12-22 06:19:54 -0800

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,12 @@
 int main(void)
 {
 	int c;
-	int boolean;
 	long int n1;
 	long int n2;
 	long int fn;
 
 	n1 = 1;
 	n2 = 2;
-	boolean = 1;
 	printf("%ld, %ld", n1, n2);
 
 	for (c = 0; c < 96; c++)
@@ -24,9 +22,6 @@ int main(void)
 	printf(", %ld", fn);
 	n1 = n2;
 	n2 = fn;
-
-	if (((n1 + n2) < 0) && boolean == 1)
-	boolean = 0;
 }
 	printf("\n");
 	return (0);
