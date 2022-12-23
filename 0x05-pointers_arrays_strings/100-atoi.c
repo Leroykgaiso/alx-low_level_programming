a664e411a67c2f93f001297f530e3b86ee89ef93
Leroykgaiso <Leroykgaiso1@hotmail.com>
2022-12-23 11:36:05 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer
+ * @s: Pointer to a character string
+ *
+ * Return: The integer value of the string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+	char *temp = s;
+
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+		{
+			sign *= -1;
+		}
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
