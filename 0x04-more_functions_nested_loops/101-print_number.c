8ac590ba5e9806dd827d8c4fbbfa2e812860c154
Leroykgaiso <Leroykgaiso1@hotmail.com>
2022-12-24 05:14:34 -0800

    number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int numb = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
+		_putchar('-');
+		numb = -num;
+		{
+		if (numb > 9)
 	{
-		print_number(i / 10);
+		print_number(num / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar(num % 10 + '0');
+		}
 }
