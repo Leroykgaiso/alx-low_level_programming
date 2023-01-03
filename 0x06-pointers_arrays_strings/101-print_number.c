dc2c906e6da4ddf197a2f6e7bf2da4a169822863
Leroykgaiso <Leroykgaiso1@hotmail.com>
2023-01-03 08:18:02 -0800

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,24 +3,21 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ * Return: 0
  */
 void print_number(int n)
 {
 	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	}
-	else
-	{
 		n1 = n;
 	}
-
-	if (n1 / 10)
+	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10);
+		print_numbers(n1 / 10);
 	}
 
 	_putchar((n1 % 10) + '0');
